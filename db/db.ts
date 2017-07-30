f2835e967d0ca2c4be26427c732d938bb5394e75
maximvanneste <maxim.vanneste@hotmail.be>
2017-07-30 16:09:37 +0200

    Clean up & added users to sql file

--- db/db.ts
+++ db/db.ts
@@ -17,40 +17,14 @@ let poolScoreboard = mysql.createPool({
 
 export { poolScoreboard };
 
-export interface ITableData {
 
-  count: number;
-  columns: string[];
-  rows: any[];
-}
-
-export function getFeedInfo(feedId: string, callback: Function) {
-  let query = 'SELECT social_query_id AS query_id, social_feeds_id AS feed_id, \
-               label, slug, networks.name AS network, uid, options, authorization, status, last_refresh \
-               FROM social_feeds \
-               LEFT JOIN social_queries ON social_feeds.social_feeds_id = social_queries.social_feed_id \
-               LEFT JOIN networks ON networks.network_id = social_queries.network_id \
-               WHERE social_feeds_id = ?';
-
-  poolScoreboard.query(query, [feedId], callback);
-}
-
-export function create(values: any, callback: (err: any, insertId?: number) => void) {
-
-  if (!values.user_id)
-    return callback('parameter not found: user_id');
-
-  if (!values.label)
-    return callback('parameter not found: label');
-
-  let qry = 'INSERT INTO social_feeds(label, slug, user_id) VALUES(?, ?, ?)';
 
   // poolScoreboard.query(qry, [values.label, slug, values.user_id], (err: any, result: any) => {
   //   if (err) return callback(err);
   //   else return callback(null, result.insertId);
   // });
 
-}
+
 
 
 export function editFeed(feed: any, callback: (err: any, insertId?: number) => void) {
@@ -81,7 +55,6 @@ export function editFeed(feed: any, callback: (err: any, insertId?: number) => v
 
 }
 
-// Cascade delete should be defined by the MySQL foreign key constraints
 export function deleteFeed(feedid: string, callback: (err: any) => void) {
 
   if (!feedid)
@@ -94,31 +67,6 @@ export function deleteFeed(feedid: string, callback: (err: any) => void) {
     else return callback(null);
   });
 }
-
-    // 2. check if user is owner of the feed
-    // function isOwner(userId: number, callback: (err: any, authInfo: any) => void) {
-    //   if (!userId || isNaN(userId))
-    //     return callback(null, {userid: userId, authorized: false});
-    //
-    //   // instantly resolve to true when no feedId is given, this means the user
-    //   // is a valid user, but not an owner of anything (yet)
-    //   if (!feedIdNumber) return callback(null, {userid: userId, authorized: true});
-    //
-    //   let qry = 'SELECT `social_feeds_id` FROM `social_feeds` \
-    //       WHERE user_id = ?';
-    //
-    //   poolScoreboard.query(qry, [userId], (err: any, rows: any[]) => {
-    //     if (err) throw err;
-    //
-    //     let feeds = _.chain(rows).map('social_feeds_id').flatten().value();
-    //     let isOwner = _.includes(feeds, feedIdNumber);
-    //
-    //     callback(null, {
-    //       userid: userId,
-    //       authorized: isOwner
-    //     });
-    //   });
-    // }
 
 
 
@@ -211,54 +159,3 @@ export function getAnswersByQuestionId(questionId: number): Promise<any[]> {
     });
   });
 }
-//
-// export function getNetwork(name: string): Promise<INetwork> {
-//   return new Promise((resolve, reject) => {
-//     const qry = `SELECT network_id as id, name FROM feeds.networks WHERE name=?;`;
-//     poolScoreboard.query(qry, [name], (err: any, result?: any) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         if (result.length > 0) {
-//           resolve(result);
-//         } else {
-//           reject(`Could not get single network from the database`);
-//         }
-//       }
-//     });
-//   });
-// }
-//
-// export function getFeedByUserId(userId: string, feedId: string): Promise<IFeedInfo[]> {
-//   return new Promise((resolve, reject) => {
-//     const qry = `SELECT
-//     social_feeds.social_feeds_id AS feed_id,
-//     social_feeds.label,
-//     social_feeds.slug,
-//     social_feeds.curation,
-//     social_queries.social_query_id,
-//     social_queries.uid,
-//     social_queries.authorization,
-//     social_queries.options,
-//     social_queries.status,
-//     social_queries.last_refresh,
-//     networks.name AS network
-//     FROM
-//     social_feeds
-//     LEFT JOIN social_queries ON social_queries.social_feed_id = social_feeds.social_feeds_id
-//     LEFT JOIN networks ON social_queries.network_id = networks.network_id
-//     WHERE
-//     social_feeds.user_id = ? AND social_feeds.social_feeds_id = ?`;
-//     poolScoreboard.query(qry, [userId, feedId], (err: any, results?: any[]) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         if (results.length > 0) {
-//           resolve(results);
-//         } else {
-//           reject(`Could not find feed for user: ${userId} & feed id: ${feedId} in the database`);
-//         }
-//       }
-//     });
-//   });
-// }
--- sql/db.sql
+++ sql/db.sql
@@ -34,3 +34,16 @@ CREATE TABLE `answers` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8$$
 
+delimiter $$
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8$$
+
+
+
